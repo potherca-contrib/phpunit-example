5822efc15d440d384d9b4d3e86821bdf4ccdae91
Ben Peachey <potherca@gmail.com>
2014-09-08 11:55:08 +0200

    Adds docblocks to User class.

--- User.php
+++ User.php
@@ -1,15 +1,31 @@
 <?php
+/**
+ * Represents a User.
+ * 
+ * Can be given a name, for easier identification.
+ * When asked to talk, will only address the entire world.
+ */
 class User {
+    /** @var string */
     protected $name;
 
+    /**
+     * return string
+     */
     public function getName() {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     */
     public function setName($name) {
         $this->name = $name;
     }
 
+    /**
+     * return string
+     */
     public function talk() {
         return "Hello world!";
     }
